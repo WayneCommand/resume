34b9e8b146dd082adfaddce7b7021ccfa9cd69d0
WayneCommand <waynecommand@gmail.com>
2023-09-05 23:26:15 +0800

    init layout.

---
+++ next.config.js
@@ -0,0 +1,9 @@
+/** @type {import('next').NextConfig} */
+
+// https://nextjs.org/docs/pages/building-your-application/deploying/static-exports
+const nextConfig = {
+    distDir: 'build',
+    output: 'export',
+}
+
+module.exports = nextConfig
